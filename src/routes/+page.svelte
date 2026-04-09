f54985e9a0d1ca5f2605b794991759a649525973
Hiroki <hoga@mit.edu>
2026-04-08 22:05:54 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
   import ScrollyMap from "$lib/ScrollyMap.svelte";
   import Narrative from "$lib/Narrative.svelte";
-  import WriteupDesign from "$lib/WriteupDesign.svelte";
-  import WriteupPlan from "$lib/WriteupPlan.svelte";
+  // import WriteupDesign from "$lib/WriteupDesign.svelte";
+  // import WriteupPlan from "$lib/WriteupPlan.svelte";
 </script>
 
 <Narrative position="before" />
 <ScrollyMap />
 <Narrative position="after" />
-<WriteupDesign />
-<WriteupPlan />
+<!-- <WriteupDesign /> -->
+<!-- <WriteupPlan /> -->
